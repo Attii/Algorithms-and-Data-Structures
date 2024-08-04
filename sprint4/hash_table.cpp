e42113b72630f2db7efd5e78535219e05ff9f308
Attii <57965097+Attii@users.noreply.github.com>
2024-08-04 21:18:34 +0200

    Description translated to english

--- sprint4/hash_table.cpp
+++ sprint4/hash_table.cpp
@@ -1,28 +1,26 @@
 // https://contest.yandex.ru/contest/24414/run-report/101881488/
 
 /*
---Принцип работы--
-Программа реализует хеш-таблицу для хранения пар ключ-значение. Она 
-оддерживает три основных операции: put, get, и delete. Хеш-таблица использует 
-массив с цепочками (списками) для разрешения коллизий. Хеш-функция 
-преобразует ключ в индекс массива, и цепочки используются для хранения элементов 
-с одинаковым хешем.
+--Principle of Operation--
+The program implements a hash table for storing key-value pairs. It supports three primary 
+operations: put, get, and delete. The hash table uses an array with chaining (lists) for collision 
+resolution. The hash function converts a key into an array index, and chains are used to store 
+elements with the same hash.
 
-Доказательство корректности
-Хеш-Функция: Используя модульное деление и abs, функция гарантирует возврат 
-корректного индекса массива, учитывая отрицательные ключи.
-Обработка Коллизий: Цепочки обеспечивают правильное разрешение коллизий, 
-позволяя хранить несколько элементов с одним хешем.
+--Proof of Correctness--
+Hash Function: By using modular division and abs, the function ensures returning the correct array 
+index, considering negative keys.
+Collision Handling: Chaining provides proper collision resolution, allowing multiple elements with 
+the same hash to be stored.
 
-Временная сложность
-put: В среднем O(1), но в худшем случае O(n), где n — количество элементов с одним хешем.
-get: Аналогично put, O(1) в среднем, O(n) в худшем случае.
-delete: Также O(1) в среднем и O(n) в худшем случае.
-Хеш-Функция: Выполняется за O(1).
+--Time Complexity--
+- put: On average O(1), but in the worst case O(n), where n is the number of elements with the same hash.
+- get: Similar to put, O(1) on average, O(n) in the worst case.
+- delete: Also O(1) on average and O(n) in the worst case.
+- Hash Function: Executes in O(1).
 
-Пространственная сложность
-Общая Пространственная Сложность: O(N), где N — максимальное количество 
-элементов, которое может быть сохранено в хеш-таблице.
+--Space Complexity--
+- Total Space Complexity: O(N), where N is the maximum number of elements that can be stored in the hash table.
 */
 
 #include <iostream>
